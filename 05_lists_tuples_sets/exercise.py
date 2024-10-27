c7f510523ac8513a0cb9d6f94b82895da46fa44e
Paul Wang <paul.wang@hp.com>
2024-10-27 15:58:18 +0800

    Add exercise for creating lists, tuples, and sets

---
+++ 05_lists_tuples_sets/exercise.py
@@ -0,0 +1,15 @@
+# List, tuples, and sets
+## this coding exercies requires you to complete three steps:
+# 1. Create a list, called `my_list`, with three elements. The total of the numbers added together should be 100.
+# 2. Create a tuple, called `my_tuple`, with a single value in it.
+# 3. Modify the variable `set2` so that set1.intersections(set2) returns {5, 77, 9,12}
+
+# 1.
+my_list = [33, 33, 34]
+
+# 2.
+my_tuple = (15,) # Note the comma after the value
+
+# 3.
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 9, 12, 77}
