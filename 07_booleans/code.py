3df77fe9dd6c137038623dc46d499ed12203a8e5
Paul Wang <paul.wang@hp.com>
2024-10-27 16:01:55 +0800

    Add examples demonstrating boolean comparisons in Python

---
+++ 07_booleans/code.py
@@ -0,0 +1,19 @@
+# Booleans in Python
+
+print(5 == 5)   # True
+
+print(5 > 5)    # False
+
+print(10 != 10) # False
+
+# Comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad)    # True
+print(friends is abroad)    # False
+
+abroad = friends
+
+print(friends == abroad)    # True
