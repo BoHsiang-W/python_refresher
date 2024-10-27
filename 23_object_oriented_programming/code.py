d3195ce2b153e3d8cdfac64ed1547274b5a7400f
Paul Wang <paul.wang@hp.com>
2024-10-27 18:21:29 +0800

    Add Student class with average calculation and example usage

---
+++ 23_object_oriented_programming/code.py
@@ -0,0 +1,28 @@
+# Object Oriented Programming
+
+student = {"name": "Rolf", "grades": (89, 90, 93, 78, 90)}
+
+def average(sequence):
+    return sum(sequence) / len(sequence)
+
+print(average(student["grades"]))   # 88.0
+print(student.average())            # AttributeError: 'dict' object has no attribute 'average'  
+
+# Object Oriented Programming
+class Student:
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+student = Student("Rolf", (89, 90, 93, 78, 90))
+print(student.name)
+print(student.grades)
+print(student.average())   # 88.0
+student2 = Student("Bob", (100, 100, 93, 78, 90))
+print(student2.name)
+print(student2.grades)
+print(student2.average())  # 92.2
+
