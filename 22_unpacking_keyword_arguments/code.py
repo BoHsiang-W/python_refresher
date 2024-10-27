28be4024f0d0b05ce10bf590e182b9fdc1e46389
Paul Wang <paul.wang@hp.com>
2024-10-27 18:12:15 +0800

    Add examples demonstrating unpacking keyword arguments in functions

---
+++ 22_unpacking_keyword_arguments/code.py
@@ -0,0 +1,40 @@
+# Unpacking arguments
+def named(**kwargs):
+    print(kwargs)
+
+named(name="Bob", age=25) # {'name': 'Bob', 'age': 25}
+
+def named(name, age):
+    print(name, age)
+
+details = {"name": "Bob", "age": 25}
+
+named(**details) # Bob 25
+
+def named(**kwargs):
+    print(kwargs)
+
+details = {"name": "Bob", "age": 25}
+named(**details) # {'name': 'Bob', 'age': 25}
+
+## Unpacking keyword arguments with print nicely
+def print_nicely(**kwargs):
+    named(**kwargs)
+    for arg, value in kwargs.items():
+        print(f"{arg}: {value}")
+
+print_nicely(name="Bob", age=25) # Bob 25
+
+## Unpacking keyword arguments with args and kwargs
+def both(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+both(1, 3, 5, name="Bob", age=25) # (1, 3, 5) {'name': 'Bob', 'age': 25}
+
+## Unpacking keyword arguments
+def my_function(**kwargs):
+    print(kwargs)
+
+my_function(**"Bob") # TypeError: unpacking argument of type 'str' is not iterable
+my_function(**None) # TypeError: 'NoneType' object is not iterable
