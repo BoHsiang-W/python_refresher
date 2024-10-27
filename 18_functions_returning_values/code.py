9c1be08ca1c34811804a79925f956dbd9511929e
Paul Wang <paul.wang@hp.com>
2024-10-27 17:06:04 +0800

    Add examples demonstrating functions returning values and basic operations

---
+++ 18_functions_returning_values/code.py
@@ -0,0 +1,30 @@
+# Functions returning values
+def add(x, y = 8):
+    print(x + y)
+    return x + y
+
+result = add(5,8)
+print(result) # 13
+
+## function returning values
+
+def add(x, y = 8):
+    return 
+    print(x + y)
+    return x + y
+
+result = add(5,8)
+print(result) # None
+
+## function returning values
+def divide(dividend, divisor):
+    if divisor != 0:
+        return dividend / divisor
+    else:
+        return "You fool!"
+
+result = divide(15, 0)
+print(result) # You fool!
+
+result = divide(15, 3) * 5
+print(result) # 25.0
---
+++ 18_functions_returning_values/exercise.py
@@ -0,0 +1,11 @@
+# functions
+## 1. complete the function return_42, by making it return 42
+## 2. create a function, my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+
+def return_42():
+    return 42
+
+def my_function(arg1, arg2):
+    return arg1 * arg2
+
+print(return_42())
