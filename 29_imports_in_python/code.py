06a18ac6fdf9e7377f61c52870e294eb865f4f39
Paul Wang <paul.wang@hp.com>
2024-10-30 22:19:08 +0800

    Add module imports and demonstrate module functionality with examples

---
+++ 29_imports_in_python/code.py
@@ -0,0 +1,14 @@
+from mymodule import divide
+
+print("code.py:", __name__)    # code.py: __main__
+print(divide(10, 2))           # 5.0
+print(__name__)                # __main__
+
+####################################################################################################
+import sys
+print(sys.path)                # finds the path of the module
+
+####################################################################################################
+import mymodule
+import sys
+print(sys.modules)             # shows all the modules that are imported
---
+++ 29_imports_in_python/libs/mylib.py
@@ -0,0 +1,1 @@
+print("mylib.py:", __name__)    # mylib.py: libs.mylib
