eb62a098f9e7c725dbb53796ca363c5de5e74489
Paul Wang <paul.wang@hp.com>
2024-10-30 22:01:23 +0800

    Add Bookshelf and Book classes demonstrating class composition

---
+++ 27_class_composition/code.py
@@ -0,0 +1,43 @@
+# Class Composition
+class Bookshelf:
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"Bookshelf with {self.quantity} books."
+    
+shelf = Bookshelf(300)
+print(shelf)    # Bookshelf with 300 books.
+
+class Book(Bookshelf):
+    def __init__(self, name, quantity):
+        super().__init__(quantity)
+        self.name = name
+
+    def __str__(self):
+        return f"Book {self.name}"
+    
+book = Book("Harry Potter", 20)
+print(book)    # Book Harry Potter
+
+####################################################################################################
+
+class Bookshelf_composition:
+    def __init__(self, *books):
+        self.books = books
+
+    def __str__(self):
+        return f"Bookshelf with {len(self.books)} books."
+
+class Book_composition:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Book {self.name}"
+    
+book1 = Book_composition("Harry Potter")
+book2 = Book_composition("Python 101")
+shelf = Bookshelf_composition(book1, book2)
+print(shelf)    # Bookshelf with 2 books.
+    
