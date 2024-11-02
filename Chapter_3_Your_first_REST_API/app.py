22f4e429a3e3d8932df7b44c223861ba7eb28f53
BoHsiang-W <bo.hsiang.wang@gmail.com>
2024-11-02 20:12:21 +0800

    Add input validation and error handling for store and item creation endpoints

--- Chapter_3_Your_first_REST_API/app.py
+++ Chapter_3_Your_first_REST_API/app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 
 app = Flask(__name__)
@@ -13,6 +14,13 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure all required fields are filled.")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists")
+
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
@@ -20,13 +28,29 @@ def create_store():
 
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400, message="Bad request. Ensure all required fields are filled.")
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists in the store")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
+
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
     items[item_id] = new_item
+
     return new_item, 201
 
 
@@ -40,7 +64,7 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -48,4 +72,4 @@ def get_items(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
