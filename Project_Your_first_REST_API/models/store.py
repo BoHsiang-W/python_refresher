fd9b27e0859bfb4ac00589c95e1164478f102c70
BoHsiang-W <bo.hsiang.wang@gmail.com>
2024-11-04 23:31:46 +0800

    Refactor models and schemas for improved relationships and clarity

--- Project_Your_first_REST_API/models/store.py
+++ Project_Your_first_REST_API/models/store.py
@@ -1,8 +1,11 @@
 from db import db
 
+
 class ItemModel(db.Model):
-    __tablename__ = 'stores'
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    
+    items = db.relationshiop(
+        "ItemModel", back_populates="store", lazy="dynamic"
+    )  # This line is used to create a relationship between the StoreModel and the ItemModel
--- Project_Your_first_REST_API/schemas.py
+++ Project_Your_first_REST_API/schemas.py
@@ -1,18 +1,22 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)  # The ID field is read-only
     name = fields.Str(required=True)  # The name field is required
     price = fields.Float(required=True)  # The price field is required
-    store_id = fields.Str(required=True)  # The store_id field is required
 
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
+class ItemSchema(PlainStoreSchema):
+    sotre_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(lambda: StoreSchema(), dump_only=True)
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(lambda: ItemSchema()), dump_only=True)  # The items field is a list of PlainItemSchema objects
