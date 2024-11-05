e1808cd8c580969fe7c5e34db4874bc3a6a7bbeb
BoHsiang-W <bo.hsiang.wang@gmail.com>
2024-11-06 00:29:03 +0800

    One-to-many relationship between stores and tags

--- Project_Your_first_REST_API/models/store.py
+++ Project_Your_first_REST_API/models/store.py
@@ -7,6 +7,8 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     items = db.relationship(
-        "ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete"
+        "ItemModel", back_populates="store", lazy="dynamic"
     )  # This line is used to create a relationship between the StoreModel and the ItemModel
-    # cascade="all, delete" is used to delete all the items associated with a store when the store is deleted
+    tags = db.relationship(
+        "TagModel", back_populates="store", lazy="dynamic"
+    )  # This line is used to create a relationship between the StoreModel and the TagModel
